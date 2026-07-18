700704048b5bb9dd4d328349c1dccb28c92a7c3b
sabin <paudelsabin0@gmail.com>
2026-07-18 22:32:30 +0545

    fix: seo

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.sabinpaudel.com.np",
-          },
-        ],
-        destination: "https://sabinpaudel.com.np/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
